addd8e074d7172ee05aabcf261248eb29e9c5187
marjana <marjana.akhter101@gmail.com>
2025-11-10 02:29:45 +0800

    lab 3

--- bmi.py
+++ bmi.py
@@ -7,8 +7,11 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     if 18.5 <= bmi <= 25.0:
         print("Normal Weight")
+        return 0
     if bmi > 25.0:
         print("Over Weight")
+        return 1
 calculate_bmi(weight=57, height=1.73)
